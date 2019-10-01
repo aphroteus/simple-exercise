cef05fac7a5c8fc4b11fd1a93ff92c26378ba9bb
Paul Huang <aphroteus@gmail.com>
2019-10-01 17:47:08 +0800

    Add number function

--- main.c
+++ main.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+int add_two_number (int num1, int num2);
+
 int main()
 {
+  int ans;
 
   printf ("Hello World\n");
 
+  ans = add_two_number(1, 2);
+  printf ("Answer = %d\n", ans);
+
   return 0;
+}
+
+
+int add_two_number (int num1, int num2)
+{
+  return (num1 + num2);
 }
