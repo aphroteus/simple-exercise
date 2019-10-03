d1c1676bcc936315bbbd3fec40f745a246303702
Paul Huang <aphroteus@gmail.com>
2019-10-03 10:54:58 +0800

    Add sub two number feature
    with a dummy bug

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int add_two_number (int num1, int num2);
+int sub_two_number (int num1, int num2);
 
 int main()
 {
@@ -12,11 +13,20 @@ int main()
   ans = add_two_number(1, 2);
   printf ("Answer = %d\n", ans);
 
+  ans = sub_two_number(1, 2);
+  printf ("Answer = %d\n", ans);
+
   return 0;
 }
 
 
 int add_two_number (int num1, int num2)
 {
+  return (num1 + num2);
+}
+
+int sub_two_number (int num1, int num2)
+{
+  DEBUG_PRINT (("DEBUG: sub_two_number entry\n"));
   return (num1 + num2);
 }
